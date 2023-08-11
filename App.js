307285159dad63000f26604449420603fef7510e
Ch.Charmi Reddy <85749261+Charmireddy@users.noreply.github.com>
2023-08-11 14:59:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './Home';
+import AddEdit from './AddEdit';
+import View from './View';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <ToastContainer position='top-center'/>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/addedit' element={<AddEdit />} />
+          <Route path='/update/:id' element={<AddEdit />} />
+          <Route path='/view/:id' element={<View />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
